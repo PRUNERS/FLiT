9e5a2c7fe21ae63318ae9be9e38eebb3df7f649a
Geof23 <fredericflintstone@gmail.com>
2015-10-08 23:58:01 -0600

    still working on remote collection

--- hostCollect.sh
+++ hostCollect.sh
@@ -16,8 +16,8 @@ make -j $1 -f Makefile2
 cd ../results
 tar -zxf *.tgz
 cat *out_ >> masterRes_$(hostname)
-git add masterRes_$(hostname)
-git commit -m 'data collecton from $(hostname)'
-git push
+# git add masterRes_$(hostname)
+# git commit -m 'data collecton from $(hostname)'
+# git push
 # $(which psql) -d qfp -h bihexal.cs.utah.edu -U sawaya -c "select importQFPResults('$PWD/masterRes');"
 exit $?
