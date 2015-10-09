89b073e1b9a88b85259bb03346ea06c2b2c350de
Geof23 <fredericflintstone@gmail.com>
2015-10-08 21:41:11 -0600

    t n+5

--- hostCollect.sh
+++ hostCollect.sh
@@ -6,11 +6,18 @@ if [[ ! -e "~/.postgresql" ]]; then
     cp /local/.postgresql/* ~/.postresql/
 fi
 
+if [[ ! -e results ]]; then
+    mkdir results
+fi
 rm results/*
 cd perpVects
 make -j $1 -f Makefile2
 
 cd ../results
-cat "*out_" >> masterRes
-$(which psql) -d qfp -h bihexal.cs.utah.edu -U sawaya -c "select importQFPResults('$PWD/masterRes');"
+tar -zxf *.tgz
+cat "*out_" >> masterRes_$(HOST)
+git add masterRes_$(HOST)
+git commit -m 'data collecton from $(HOST)'
+git push
+# $(which psql) -d qfp -h bihexal.cs.utah.edu -U sawaya -c "select importQFPResults('$PWD/masterRes');"
 exit $?
