cd64dffc1d5ad892246569569a48c016f138061a
Geof23 <fredericflintstone@gmail.com>
2015-10-08 18:24:47 -0600

    still massaging remote collect

--- hostCollect.sh
+++ hostCollect.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
+#this is for Cloudlab
 if [[ ! -e "~/.postgresql" ]]; then
     mkdir ~/.postgresql
     cp /local/.postgresql/* ~/.postresql/
 fi
-cd ~/qfp/results
+
+rm results/*
+git stash
+git checkout master
+git pull
+cd perpVects
+make -j $1 -f Makefile2
+
+cd ../results
 cat "*out_" >> masterRes
 $(which qfp) -c "select importQFPResults('$PWD/masterRes');"
 exit $?
