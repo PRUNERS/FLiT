12e4b518dc440d32f0e40fb033b8239f5624bc87
Geof23 <fredericflintstone@gmail.com>
2015-10-13 03:35:01 -0600

    trying to eliminate error in archive target

--- perpVects/test.cpp
+++ perpVects/test.cpp
@@ -809,7 +809,8 @@ struct FPTests {
 	}else{
 	  if(i == 0) score += fabs(a ^ b);  //if falsely not detecting ortho, should be the dot prod
 	}
-	info_stream << "a[" << r << "] = " << a[r] << " perp: " << isOrth << " dot prod: " << (a ^ b) endl;
+	info_stream << "a[" << r << "] = " << a[r] << " perp: " << isOrth << " dot prod: " <<
+	  FPWrap<T>(a ^ b) << endl;
       }
       info_stream << "next dimension . . . " << endl;
       p = backup;
