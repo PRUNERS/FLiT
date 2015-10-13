54e55445d8d844b8b6e1d8266b98d0ff72de64c5
Geof23 <fredericflintstone@gmail.com>
2015-10-13 02:48:39 -0600

    woops fixed error

--- hostCollect.sh
+++ hostCollect.sh
@@ -13,7 +13,7 @@ rm results/*
 cd perpVects
 make -j $1 -f Makefile2
 
-if [[ $VERBOSE == 'verbose' ]]; then
+if [[ $VERBOSE != 'verbose' ]]; then
     cd ../results
     tar -zxf *.tgz
     cat *out_ >> masterRes_$(hostname)
