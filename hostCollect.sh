f9e67bcff4712f5370c2c2c341050aea43909b3a
Geof23 <fredericflintstone@gmail.com>
2015-10-13 02:47:46 -0600

    fixing verbose run

--- hostCollect.sh
+++ hostCollect.sh
@@ -13,11 +13,14 @@ rm results/*
 cd perpVects
 make -j $1 -f Makefile2
 
-cd ../results
-tar -zxf *.tgz
-cat *out_ >> masterRes_$(hostname)
-# git add masterRes_$(hostname)
-# git commit -m 'data collecton from $(hostname)'
-# git push
-# $(which psql) -d qfp -h bihexal.cs.utah.edu -U sawaya -c "select importQFPResults('$PWD/masterRes');"
-exit $?
+if [[ $VERBOSE == 'verbose' ]]; then
+    cd ../results
+    tar -zxf *.tgz
+    cat *out_ >> masterRes_$(hostname)
+    # git add masterRes_$(hostname)
+    # git commit -m 'data collecton from $(hostname)'
+    # git push
+    # $(which psql) -d qfp -h bihexal.cs.utah.edu -U sawaya -c "select importQFPResults('$PWD/masterRes');"
+    exit $?
+fi
+exit 0
