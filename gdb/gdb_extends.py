fd197126ebbf210341e2bddac0b4d2f0a4013b6e
Geof23 <fredericflintstone@gmail.com>
2015-12-03 04:22:30 -0700

    Doing some major work for the differential debugging. At this point, I'm diverging from my plan -- was going to run in tandem and check at every watch hit, but this is too expensive due to synchronization issues.  Also was going to use gdb checkpoints at each step, but this is way too expensive (naive also).  Now we will do some number of hits per cycle, and then synchronize the inferiors at this point (like 100?).  Then we'll compare the recorded writes and if one (earliest) is detected, we'll rerun each to that point (with record then enabled).  This only requires synchronization every N hits.

--- gdb/gdb_extends.py
+++ gdb/gdb_extends.py
@@ -4,40 +4,51 @@
 # a valid inferior
 
 import gdb
-from os import environ
+import os
+import sys
+sys.path.append(os.getcwd())
 import helpers
 
-if len(gdb.inferiors()) != 1 || !gdb.selected_inferior().is_valid():
-    print('in ' + __file__ + ' should have one valid inferior, found 0, quitting . . .')
-    gdb.execute('quit')
+#data structures
+
+
+
+# if len(gdb.inferiors()) != 1 || !gdb.selected_inferior().is_valid():
+#     print('in ' + __file__ + ' should have one valid inferior, found 0, quitting . . .')
+#     gdb.execute('quit')
 
 # some data structures. 
 ## get needed environment info
-precision = getPrecString(os.environ.get("PREC"))
-test = os.environ.get("TEST")
+# precision = getPrecString(os.environ.get("PREC"))
+# test = os.environ.get("TEST")
 ##this is our list of critical data, one list per test
-criticals = [['Globals<' + precision + '>::sum', '*(' + precision + '*)' +
-              lookupAddress('Globals<' + precision + '>::prods[0]']),
-              ['Globals<' + precision + '>::sum', '*(' + precision + '*)' +
-               lookupAddress('Globals<' + precision + '>::prods[0]')]]
+# criticals = [['Globals<' + precision + '>::sum', '*(' + precision + '*)' +
+#               lookupAddress('Globals<' + precision + '>::prods[0]']),
+#               ['Globals<' + precision + '>::sum', '*(' + precision + '*)' +
+#                lookupAddress('Globals<' + precision + '>::prods[0]')]]
               
 # the initialization -- load other inferior and setup rewind (i.e. record)
+
+gdb.events.stop.connect(helpers.catch_trap)
+
+helpers.execCommands(['run 2> inf1.watch', 'add-inferior -exec inf2',
+                      'inferior 2', 'run 2> inf2.watch'])
               
-init_commands = ['add-inferior -exec file2', 'break main', 'run', 'inferior 2', 'run', 'record', 'delete 1']
+# init_commands = ['add-inferior -exec inf2', 'run', 'inferior 2', 'run', 'record']
 
-execCommands(init_commands)
+# execCommands(init_commands)
 
-#next we set up the watchpoints
-## here we need to use the python extensions for our
-## custom qfpWatchpoint and registering callbacks 
-execCommands(['inferior 1'])
-for c in criticals[test]:
-    qfpWatchpoint.__init__(c)
-execCommands(['inferior 2'])
-for c in criticals[test]:
-    qfpWatchpoint.__init__(c)
+# #next we set up the watchpoints
+# ## here we need to use the python extensions for our
+# ## custom qfpWatchpoint and registering callbacks 
+# execCommands(['inferior 1'])
+# for c in criticals[test]:
+#     qfpWatchpoint.__init__(c)
+# execCommands(['inferior 2'])
+# for c in criticals[test]:
+#     qfpWatchpoint.__init__(c)
                  
-#we should be ready to go now, and let the watchpoints do their work
+# #we should be ready to go now, and let the watchpoints do their work
 
-execCommands(['run', 'inferior 1', 'run'])
+# execCommands(['run', 'inferior 1', 'run'])
 
