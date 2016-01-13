fa19663ed2c14d3298537c6e3c6b8879bcd6cc4c
Geof Sawaya <sawaya@c557-202.stampede.tacc.utexas.edu>
2016-01-13 07:05:31 -0600

    cleaned up the real mccoy.  This will produce the different results of division between Phi and Xeon-Phi

--- quickMicTest/mccoy/test3.cpp
+++ quickMicTest/mccoy/test3.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cstdlib>
+#include <cmath>
 
 
 int
@@ -12,10 +13,18 @@ main(int argc, char* argv[]){
   long double c3 = atof(argv[2]);
   std::cout << "parsed two double values from input" << std::endl;
   std::cout.precision(200);
-  std::cout << "float p / c is: " << p1 / c1 << std::endl;
-
-  std::cout << "double p / c is: " << p2 / c2 << std::endl;
-
-  std::cout << "long double p / c is: " << p3 / c3 << std::endl;
+  float fa = p1/c1;
+  double da = p2/c2;
+  long double lda = p3/c3;
+  int exp;
+  std::cout << "float p / c is: " << fa << std::endl;
+  // float fsig = frexp(fa, &exp);
+  // std::cout << "fa exp: " << exp << ", mantissa: " << fsig << std::endl;
+  std::cout << "double p / c is: " << da << std::endl;
+  // double dsig = frexp(da, &exp);
+  // std::cout << "da exp: " << exp << ", mantissa: " << dsig << std::endl;
+  std::cout << "long double p / c is: " << lda << std::endl;
+  // long double ldsig = frexp(lda, &exp);
+  // std::cout << "lda exp: " << exp << ", mantissa: " << ldsig << std::endl;
 
 }
