efece4be55b657d7694db1e36f4cf663a756c946
Geof23 <fredericflintstone@gmail.com>
2016-03-09 18:24:40 -0700

    OK, we now have a working version of QC and QD with KGen integration. Now we need to generate a bunch of data using these and a coherent interface for viewing / exploring / understanding the results.

--- ncar/MG2_CAM5_INTEL/testQD.bash
+++ ncar/MG2_CAM5_INTEL/testQD.bash
@@ -1,1 +1,1 @@
-../../gdb/doDiffKgen.py kernel.intel kernel.gcc packed_dei 9 17 /uufs/chpc.utah.edu/common/home/u0422778/qfp/ncar/MG2_CAM5_INTEL/micro_mg_tend2_0.10.300 f
+../../gdb/doDiffKgen.py kernel.i.qd kernel.g.qd packed_dei 9 17 /uufs/chpc.utah.edu/common/home/u0422778/qfp/ncar/MG2_CAM5_INTEL/micro_mg_tend2_0.50.300 t
