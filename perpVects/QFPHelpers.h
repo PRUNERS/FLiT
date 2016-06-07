8ae8d2d51ad28bc9304a093586cb960ceab3a8af
Geof23 <fredericflintstone@gmail.com>
2016-06-06 21:35:47 -0600

    This has the refactored and redesigned test system. Previous commits were missing the new files. This includes the following features: * test superclass * macro test class factory * access mechanism for watched data point (including value stack) This does not have all the litmus tests incorporated into the new test structure.

---
+++ perpVects/QFPHelpers.h
@@ -0,0 +1,622 @@
+// the header for QFP helpers.  These classes, such as matrix and
+// vector, utilize the testBase watch data items for monitoring by
+// differential debugging.
+
+#ifndef QFPHELPERS
+#define QFPHELPERS
+
+#include <ostream>
+#include <iostream>
+#include <type_traits>
+#include <float.h>
+#include <random>
+#include <algorithm>
+
+//#include "testBase.h"
+
+namespace QFPHelpers {
+
+const int RAND_SEED = 1;
+const bool NO_SUBNORMALS = true;
+
+extern std::default_random_engine gen;
+
+//used for directing output to nowhere if wanted
+class InfoStream : public std::ostream{
+  class NullBuffer : public std::streambuf
+  {
+  public:
+    int overflow(int c) { return c; }
+  };
+public:
+  InfoStream():std::ostream(new NullBuffer()){}
+  void
+  reinit(std::streambuf* b){init(b);}
+  void
+  show(){reinit(std::cout.rdbuf());}
+  void
+  hide(){reinit(new NullBuffer());}
+};
+
+static void printOnce(std::string, void*);
+
+extern InfoStream info_stream;
+  
+//returns a bitlength equivalent unsigned type
+template<typename T>
+struct get_corresponding_type{
+  using type = typename std::conditional<
+    std::is_floating_point<T>::value && sizeof(T) == 4 , uint32_t,
+    typename std::conditional<std::is_floating_point<T>::value && sizeof(T) == 8 , uint64_t,
+		     typename std::conditional<std::is_floating_point<T>::value && sizeof(T) == 16 ,
+				      unsigned __int128, void>::type>::type>::type;
+};
+
+std::ostream& operator<<(std::ostream&, const unsigned __int128);
+
+struct FPHelpers {
+  static const unsigned expBitWidth32 = 8;
+  static const unsigned expBitWidth64 = 11;
+  static const unsigned expBitWidth80 = 15;
+  static const unsigned mantBitWidth32 = FLT_MANT_DIG; //32 - expBitWidth32 - 1;
+  static const unsigned mantBitWidth64 = DBL_MANT_DIG; //64 - expBitWidth64 - 1;
+  static const unsigned mantBitWidth80 = LDBL_MANT_DIG; //128 - expBitWidth80 - 1;
+  static const unsigned bias32 = (1 << expBitWidth32 - 1) - 1;
+  static const unsigned bias64 = (1 << expBitWidth64 - 1) - 1;
+  static const unsigned bias80 = (1 << expBitWidth80 - 1) - 1;
+  
+  template<typename S, typename R>
+  static void
+  projectType(S const &source, R& result){
+    S temp = source;
+    R* u = reinterpret_cast<R*>(&temp);
+    if( sizeof(S) == 16 && std::is_floating_point<S>::value){ //we must be using long double, which is 80 bit extended -- mask out higher bits
+      unsigned __int128 zero = 0;
+      //some compilers are leaving garbage in unused bits (not defined behavior)
+      *u = (unsigned __int128)*u & (~zero >> 48); //this is really tacky hacky
+    }
+    result = *u;
+  }
+  
+  template<typename F>
+  static typename get_corresponding_type<F>::type
+  projectType(F const &source){
+    using I = typename get_corresponding_type<F>::type;
+    F temp = source;
+    I* u = reinterpret_cast<I*>(&temp);
+    if( sizeof(F) == 16 && std::is_floating_point<F>::value){ //we must be using long double, which is 80 bit extended -- mask out higher bits
+      unsigned __int128 zero = 0;
+      *u = *u & (~zero >> 48);
+    }
+    return *u;
+  }
+
+  //the first normalized number > 0 (the smallest positive) -- can be obtained from <float>
+  //[pos][bias + 1][000...0]
+  template<typename T>
+  static T
+  getFirstNorm(){
+    T retVal;
+    typedef typename get_corresponding_type<T>::type t;
+    t val;
+    switch(sizeof(T)){
+    case 4:
+      {
+	val = (t)1 << mantBitWidth32;
+      }
+      break;
+    case 8:
+      {
+	val = (t)1 << mantBitWidth64;
+	projectType(val, retVal);
+      }
+      break;
+    case 16:
+      {
+	val = (t) 1 << mantBitWidth80;
+      }
+      break;
+    default:
+      //won't compile
+      int x = 0;
+    }
+    projectType(val, retVal);
+    return retVal;
+  }
+
+  template<typename T>
+  static T
+  perturbFP(T const &src, unsigned offset){ //negative offset with 0 may produce NAN
+    T ncSrc = src;
+    typename get_corresponding_type<T>::type tmp;
+    if(NO_SUBNORMALS && ncSrc == 0) ncSrc = getFirstNorm<T>();
+    T retVal = perturbFPTyped(ncSrc, tmp, offset);
+    return retVal;
+  }
+
+  template<typename T, typename U>
+  static T
+  perturbFPTyped(T &src, U &tmp, int offset){
+    T retVal;
+    projectType(src, tmp);
+    tmp += offset;
+    projectType(tmp, retVal);
+    return retVal;
+  }
+
+  //returns the exponent portion of floating point
+  template<typename T>
+  static unsigned
+  getExponent(T v){
+    unsigned retVal = -1;
+    typename get_corresponding_type<T>::type val;
+    projectType(v, val);
+    switch(sizeof(v)){
+    case 4:
+      {
+	retVal = ((val >> (32 - expBitWidth32 - 1) & 0x7F) - bias32);
+      }
+      break;
+    case 8:
+      {
+	retVal = ((val >> (64 - expBitWidth64 - 1) & 0x7FF) - bias64); 
+      }
+      break;
+    case 16:
+      {
+	retVal = ((val >> (80 - expBitWidth80 - 1) & 0x7FFF) - bias80);
+      }
+      break;
+    default:
+      retVal = 0;
+    }
+    return retVal;
+  }
+
+
+};
+
+template<typename F>
+struct FPWrap{
+  using I = typename get_corresponding_type<F>::type;
+  F const &floatVal;
+  I intVal;
+  void
+  update() const {
+    FPHelpers::projectType(floatVal, const_cast<FPWrap*>(this)->intVal);
+  }
+
+  FPWrap(F const &val):floatVal(val){}
+  template<typename U>
+  friend std::ostream& operator<<(std::ostream& os, FPWrap<U> const &w);
+};
+
+template <typename U>
+std::ostream& operator<<(std::ostream& os, const FPWrap<U> &w){
+  w.update();
+  std::ios_base::fmtflags f = os.flags();
+  //FIXME can't handle 128 bit values for ostream operations
+  os << std::hex << w.intVal;
+  os.flags(f);
+  return os;
+}
+
+ enum sort_t{
+   lt, //manual lt magnitude sort
+   gt, //manual gt magnitude sort
+   bi, //built-in (std::inner_product) [only for ^, assumes def otherwise]
+   def //default (manual unsorted)
+ };
+
+template<typename T>
+class Matrix;
+ 
+template <typename T>
+class Vector {
+  std::vector<T> data;
+public:
+  Vector():data(0){}
+  Vector(std::initializer_list<T> l):data(l){}
+  Vector(size_t size):data(size, 0){}
+  template <typename U>
+  Vector(size_t size, U genFun):data(size, 0){
+    std::generate(data.begin(), data.end(), genFun);
+  }
+  Vector(const Vector &v):data(v.data) {}
+  size_t
+  size() const {return data.size();}
+
+  T&
+  operator[](size_t indx){return data[indx];}
+
+  const T&
+  operator[](size_t indx) const {return data[indx];}
+
+  Vector<T>
+  operator*(T const &rhs){
+    Vector<T> retVal(size());
+    for(int x = 0; x < size(); ++x){
+      retVal[x] = data[x] * rhs;
+    }
+    return retVal;
+  }
+
+  static
+  Vector<T>
+  getRandomVector(size_t dim, T min_inc, T max_exc,
+		  std::default_random_engine::result_type seed = 0,
+		  bool doSeed = false){
+    if(doSeed) gen.seed(seed);
+    std::uniform_real_distribution<T> dist(min_inc,max_exc);
+    Vector<T> tmp(dim);
+    for(auto& i: tmp.data){
+      i = dist(gen);
+    }
+    return tmp;
+  }
+
+  Vector<T>
+  getUnitVector() const {
+    Vector<T> retVal(*this);
+    return retVal * ((T)1.0 / (this->L2Norm()));
+  }
+
+  bool
+  operator==(Vector<T> const &b){
+    bool retVal = true;
+    for(int x = 0; x < size(); ++x){
+      if(data[x] != b.data[x]){
+	retVal = false;
+	break;
+      }
+    }
+    return retVal;
+  }
+  void
+  dumpDistanceMetrics(Vector<T> const &b, std::ostream& os){
+    os << "vector difference: \t" << ((*this) - b) << std::endl;
+    os << "l1distance:  \t" << (*this).L1Distance(b) << std::endl;
+  }
+
+  //creates a vector with randomly swapped elements, every other
+  //one negated.  For odd sized vectors, the odd one out will
+  //be zero
+  //i.e.
+  // 1  2 3  4
+  // may produce
+  // 2 -1 4 -3
+  //or
+  // 1 2  3  4 5
+  // may produce
+  // 3 4 -1 -2 0
+  Vector<T>
+  genOrthoVector(){
+    Vector<T> retVal(size());
+    std::vector<T> seq(size());
+    iota(seq.begin(), seq.end(), 0); //load with seq beg w 0
+    shuffle(seq.begin(), seq.end(), std::default_random_engine(RAND_SEED));
+    //do pairwise swap
+    for(int i = 0; i < size(); i += 2){
+      retVal[seq[i]] = data[seq[i+1]];
+      retVal[seq[i+1]] = -data[seq[i]];
+    }
+    if(size() & 1) //odd
+      retVal[seq[size() - 1]] = 0;
+    return retVal;
+  }
+
+  T
+  getLInfNorm(){
+    T retVal;
+    std::pair<int, T> largest;
+    for(int x = 0; x < data.size(); ++x){
+      T abe = fabs(data[x]);
+      if(abe > largest.second){
+	largest.first = x;
+	largest.second = abe;
+      }
+    }
+  }
+
+  void
+  rotateAboutZ_3d(T rads){
+    Matrix<T> t = {{cos(rads), -sin(rads), 0},
+		{sin(rads), cos(rads), 0},
+		{0, 0, 1}};
+    info_stream << "rotation matrix is: " << t << std::endl;
+    Vector<T> tmp(*this);
+    tmp = t * tmp;
+    info_stream << "in rotateAboutZ, result is: " << tmp << std::endl;
+    data = tmp.data;
+    
+  }
+
+  Vector<T>
+  operator-(Vector<T> const &rhs) const {
+    Vector<T> retVal(size());
+    for(int x = 0; x < size(); ++x){
+      retVal[x] = data[x] - rhs.data[x];
+    }
+    return retVal;
+  }
+
+  unsigned __int128
+  BitDistance(Vector<T> const &rhs) const {
+    typedef unsigned __int128 itype;
+    itype retVal;
+    for(int i = 0; i < size(); ++i){
+      retVal += std::labs((itype)FPWrap<long double>((long double)data[i]).intVal - FPWrap<T>((long double)rhs.data[i]).intVal);
+    }
+    return retVal;
+  }
+
+  //may be more useful than L2 as there is no square/sqrt
+  //op to lose bits
+  T
+  L1Distance(Vector<T> const &rhs) const {
+    T distance = 0;
+    for(int x = 0; x < size(); ++x){
+      distance += fabs(data[x] - rhs.data[x]);
+    }
+    return distance;
+  }
+
+  //method to reduce vector (pre-sort)
+
+private:
+  sort_t sortType = def;
+public:
+  template<class U>
+  friend std::ostream& operator<<(std::ostream& os, Vector<U> const &a);
+
+  //fun is lamda like: [&sum](T a){sum += a};
+  //you provide the T sum, closure will capture
+  //not const because it may sort cont
+  //C is container type
+
+  template<typename F, class C>
+  void
+  reduce(C &cont, F const &fun) const {
+    //    printOnce(__FUNCTION__, &cont.data()[0]);
+
+    T retVal;
+    if(sortType == lt || sortType == gt){
+      if(sortType == lt)
+	std::sort(cont.begin(), cont.end(),
+		  [](T a, T b){return fabs(a) < fabs(b);});
+      else
+	std::sort(cont.begin(), cont.end(),
+		  [](T a, T b){return fabs(a) > fabs(b);});
+    }
+    for_each(cont.begin(), cont.end(), fun);
+  }
+
+  void
+  setSort(sort_t st = def){sortType = st;}
+  //inner product (dot product)
+  T
+  operator^(Vector<T> const &rhs) const {
+    T sum = 0.0;
+    auto prods = std::vector<T>(sizeof(data.size()));
+    T temp = 0.0;
+    if( sortType == bi){
+      sum = std::inner_product(data.begin(), data.end(), rhs.data.begin(), (T)0.0);
+    }else{
+      std::for_each(prods.begin(), prods.end(), [&](T i){i = 0.0;});
+      for(int i = 0; i < size(); ++i){ 
+	prods[i] = data[i] * rhs.data[i]; 
+      }
+      for(auto j:prods){
+	temp += j;
+      }
+    }
+    return (sum = temp);
+  }
+
+  T
+  LInfNorm() const {
+    T retVal = 0;
+    for(auto e: data){
+      T tmp = fabs(e);
+      if( tmp > retVal) retVal = tmp;
+    }
+    return retVal;
+  }
+
+  T LInfDistance(Vector<T> const &rhs) const {
+    T retVal;
+    auto diff = operator-(rhs);
+    return diff.LInfNorm();
+  }
+
+  //L2 norm
+  T
+  L2Norm() const {
+    Vector<T> squares(size());
+    T retVal = 0;
+    std::vector<T> prods(data); //make a copy of our data for possible sorted reduction
+    //will sort reduction if sort
+    reduce(prods, [&retVal](T e){retVal += e*e;});
+    return std::sqrt(retVal);
+  }
+
+  T
+  L2Distance(Vector<T> const &rhs) const {
+    T retVal = 0;
+    auto diff = operator-(rhs);
+    //sorts pre-rediction for sortType = gt|lt
+    reduce(diff.data, [&retVal](T e){retVal += e*e;});
+    return std::sqrt(retVal);
+  }
+ 
+  //cross product, only defined here in 3d
+  Vector<T>
+  cross(Vector<T> const &rhs) const {
+    Vector<T> retVal(size());
+    retVal.data[0] = data[1] * rhs.data[2] - rhs.data[1] * data[2];
+    retVal.data[1] = rhs.data[0] * data[2] - data[0] * rhs.data[2];
+    retVal.data[2] = data[0] * rhs.data[1] - rhs.data[0] * data[1];
+    return retVal;
+  }
+  
+  Vector<T>
+  operator*(Vector<T> const &rhs) const {
+    Vector<T> retVal(size());
+    for(int x = 0; x < size(); ++x){
+      retVal[x] = data[x] * rhs.data[x];
+    }
+    return retVal;
+  }
+
+  bool
+  isOrtho(Vector<T> const &rhs){
+    return operator^(rhs) == (T)0;
+  }
+
+};
+template <typename T>
+std::ostream& operator<<(std::ostream& os, Vector<T> const &a){
+  for(auto i: a.data){
+    os << i << '\t';
+  }
+  return os;
+}
+
+template <typename T>
+std::ostream& operator<<(std::ostream& os, Matrix<T> const &m){
+  for(auto r: m.data){
+    for(auto i: r){
+      os << i << '\t';
+    }
+    os << std::endl;
+  }
+  return os;
+}
+  
+template<typename T>
+class Matrix {
+  std::vector<std::vector<T>> data;
+public:
+  Matrix(unsigned rows, unsigned cols):
+    data(rows, std::vector<T>(cols, 0)){}
+  Matrix(Matrix<T> const &m):data(m.data){}
+  Matrix(std::initializer_list<std::initializer_list<T>> l):
+    data(l.size(), std::vector<T>(l.begin()->size())){
+    int x = 0; int y = 0;
+    for(auto r: l){
+      for(auto i: r){
+	data[x][y] = i;
+	++y;
+      }
+      ++x; y = 0;
+    }
+  }
+  
+  friend class Vector<T>;
+  template<class U>
+  friend std::ostream& operator<<(std::ostream& os, Matrix<U> const &a);
+
+
+  bool
+  operator==(Matrix<T> const &rhs){
+    bool retVal = true;
+    for(int x = 0; x < data.size(); ++x){
+      for(int y = 0; y < data[0].size(); ++y){
+	if(data[x][y] != rhs.data[x][y]){
+	  info_stream << "in: " << __func__ << std::endl;
+	  info_stream << "for x,y: " << x << ":" << y << std::endl;
+	  info_stream << "this = " << data[x][y] << "; rhs = " << rhs.data[x][y] << std::endl;
+	  retVal = false;
+	  break;
+	}
+      }
+    }
+    return retVal;
+  }
+
+  Matrix<T>
+  operator*(T const &sca){
+    Matrix<T> retVal(data.size(), data[0].size());
+    for(int x = 0; x < data.size(); ++x){
+      for(int y =0; y < data[0].size(); ++y){
+	retVal.data[x][y] = data[x][y] * sca;
+      }
+    }
+    return retVal;
+  }
+  
+  //precond: this.w = rhs.h, duh
+  Matrix<T>
+  operator*(Matrix<T> const &rhs){
+    Matrix<T> retVal(data.size(), rhs.data[0].size());
+    for(int bcol = 0; bcol < rhs.data[0].size(); ++bcol){
+      for(int x = 0; x < data.size(); ++x){
+	for(int y = 0; y < data[0].size(); ++y){
+	  retVal.data[x][bcol] += data[x][y] * rhs.data[y][bcol];
+	}
+      }
+    }
+    return retVal;
+  }
+
+  //precond: dim(v) == 3
+  static
+  Matrix<T>
+  SkewSymCrossProdM(Vector<T> const &v){
+    return Matrix<T>(
+      {{0, -v[2], v[1]},
+	  {v[2], 0, -v[0]},
+	    {-v[1], v[0], 0}});
+  }
+
+  static
+  Matrix<T>
+  Identity(size_t dims){
+    Matrix<T> retVal(dims, dims);
+    for(int x = 0; x < dims; ++x){
+      for(int y =0; y < dims; ++y){
+	if(x == y) retVal.data[x][y] = 1;
+	else retVal.data[x][y] = 0;
+      }
+    }
+    return retVal;
+  }
+  
+  Vector<T>
+  operator*(Vector<T> const &v) const {
+    info_stream << "in Matrix multiply operator with matrix:" << std::endl;
+    info_stream << (*this);
+    Vector<T> retVal(data.size());
+    int resI = 0;
+    for(auto row: data){
+      for(size_t i = 0; i < row.size(); ++i){
+	retVal[resI] += row[i] * v[i];
+      }
+      ++resI;
+    }
+    return retVal;
+  }
+
+  Matrix<T>
+  operator+(Matrix<T> const&rhs) const{
+    Matrix<T> retVal(rhs);
+    int x = 0; int y = 0;
+    for(auto r: data){
+      for(auto i: r){
+	retVal.data[x][y] = i + rhs.data[x][y];
+	++y;
+      }
+      y = 0; ++x;
+    }
+    return retVal;
+  }
+
+  void
+  print() const {
+    std::cout << *this;
+  }
+};
+
+}
+
+#endif
---
+++ perpVects/testBase.h
@@ -0,0 +1,74 @@
+// This contains the base class for tests.
+// It includes a 'register' static method for a factory style
+// instantiation of tests.
+
+#ifndef TESTBASE
+#define TESTBASE
+
+#include <map>
+#include <string>
+
+#include "QFPHelpers.h"
+
+namespace QFPTest {
+  
+void setWatching(bool watch = true);
+
+typedef std::pair<std::string, std::pair<long double, long double> > resultType;
+
+  
+struct testInput {
+  size_t iters;
+  size_t highestDim;
+  size_t ulp_inc;
+  float min;
+  float max;
+  QFPHelpers::sort_t reduction_sort_type;
+};
+
+template<typename T>
+void pushWatchData();
+
+template<typename T>
+void popWatchData();  
+
+template<typename T>
+volatile T&  getWatchData();
+
+class TestBase;
+
+class TestFactory{
+ public:
+  virtual TestBase *create() = 0;
+};
+
+class TestBase {
+public:
+ TestBase(std::string id):id(id){}
+  static inline
+    void registerTest(const std::string& name, TestFactory *factory){
+    tests[name] = factory;
+  }
+  
+  virtual resultType floatTest(const testInput&) = 0;
+  virtual resultType doubleTest(const testInput&) = 0;
+  virtual resultType longTest(const testInput&) = 0;
+  static std::map<std::string, TestFactory*> tests;  
+private:
+  std::string id;
+};
+
+ 
+#define REGISTER_TYPE(klass) \
+  class klass##Factory : public TestFactory  {			  \
+  public:					  \
+    klass##Factory() {				  \
+      TestBase::registerTest(#klass, this);	  \
+    } \
+    virtual TestBase *create(){ \
+      return new klass(); \
+    } \
+  }; \
+  static klass##Factory global_##klass##Factory;
+}
+#endif
