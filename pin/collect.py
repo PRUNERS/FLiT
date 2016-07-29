0720b48eba64269805bc66490114b5699bd51a5d
Geof23 <fredericflintstone@gmail.com>
2016-07-29 01:03:57 -0600

    Getting ready for a pin data extraction of machine inst count

---
+++ pin/collect.py
@@ -0,0 +1,37 @@
+#this script runs the pin opcodemix to collect QC results.
+#It works in the results directory on a host that ran QC (wihtout deleting output)
+
+import glob
+import sys
+
+if not os.path.exists('opcodes'):
+    os.makedirs('opcodes')
+
+for f in glob.glob('*'):
+    if f[:-4] == 'out_':
+        continue
+    splt = f.split('_')
+    if len(split) != 3:
+        continue
+    compiler = split[0]
+    host = split[1]
+    flags = split[2]
+    for p in ['f', 'd', 'e']:
+        os.environ['PRECISION'] = p
+        for t in ['DistributivityOfMultiplication',
+                  'RotateAndUnrotate',
+                  'DoSkewSymCPRotationTest',
+                  'DoHariGSBasic',
+                  'DoHariGSImproved',
+                  'DoSimpleRotate90',
+                  'TrianglePHeron',
+                  'DoMatrixMultSanity',
+                  'DoOrthoPerturbTest',
+                  'TrianglePSylv',
+                  'RotateFullCircle',
+                  'DistributivityInMultiplication']:
+
+            os.environ['TEST'] = t
+            print(check_output(['../pin/*/pin', '-t ' +
+                                '../pin/*/source/tools/SimpleExamples/obj-intel64/opcodemix.so',
+                                '-o ' + 'opcodes/' + f + '_' + p + '_' + t + '.pin', '--', f]))
