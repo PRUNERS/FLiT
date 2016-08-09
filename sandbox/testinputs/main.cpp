b2484ab9eafb25aefc295c048e2b8f3836d56ca1
Michael Bentley <mikebentley15@gmail.com>
2016-08-09 17:26:46 -0600

    testinputs: Make it work with float, double, and long double

--- sandbox/testinputs/main.cpp
+++ sandbox/testinputs/main.cpp
@@ -1,16 +1,86 @@
 #include "groundtruth.h"
 #include "test.h"
 
+#include <typeinfo>
 #include <random>
 #include <tuple>
 #include <iostream>
 #include <cmath>
+#include <type_traits>
 
 template <typename ToType, typename FromType>
 ToType my_reinterpret(FromType val) {
   return *reinterpret_cast<ToType*>(&val);
 }
 
+
+// returns a bitlength equivalent unsigned type for floats
+// and a bitlength equivalent floating type for integral types
+template<typename T>
+struct get_corresponding_type {
+  using type = typename std::conditional_t<
+    std::is_floating_point<T>::value && sizeof(T) == 4, uint32_t,
+    std::conditional_t<
+      std::is_floating_point<T>::value && sizeof(T) == 8, uint64_t,
+		std::conditional_t<
+      std::is_floating_point<T>::value && sizeof(T) == 16, unsigned __int128,
+    std::conditional_t<
+      std::is_integral<T>::value && sizeof(T) == sizeof(float), float,
+    std::conditional_t<
+      std::is_integral<T>::value && sizeof(T) == sizeof(double), double,
+    std::conditional_t<
+      std::is_integral<T>::value && sizeof(T) == sizeof(long double), long double,
+    std::conditional_t<
+      std::is_same<T, __int128>::value && sizeof(long double) == 16, long double,
+    std::conditional_t<
+      std::is_same<T, unsigned __int128>::value && sizeof(long double) == 16, long double,
+      void
+    >>>>>>>>;
+};
+
+template <typename T>
+using get_corresponding_type_t = typename get_corresponding_type<T>::type;
+
+/**
+ * Reinterpret float to integral or integral to float
+ *
+ * The reinterpreted float value will be an unsigned integral the same size as the passed-in float
+ *
+ * The reinterpreted integral value will be a floating point value of the same size
+ *
+ * Example:
+ *   auto floatVal = reinterpret_convert(0x1f3742ae);
+ *   auto intVal   = reinterpret_convert(floatVal);
+ *   printf("%e\n", floatVal);
+ *   printf("0x%08x\n", intVal);
+ * Output:
+ *   3.880691e-20
+ *   0x1f3742ae
+ */
+template<typename F>
+get_corresponding_type_t<F>
+reinterpret_convert(F val) {
+  using ToType = get_corresponding_type_t<F>;
+  return *reinterpret_cast<ToType*>(&val);
+}
+
+/** Convenience for reinterpret_convert().  Enforces the type to be integral */
+template<typename I>
+decltype(auto) reinterpret_int_as_float(I val) {
+  static_assert(std::is_integral<I>::value
+                  || std::is_same<I, __int128>::value
+                  || std::is_same<I, unsigned __int128>::value,
+                "Must pass an integral type to reinterpret as floating-point");
+  return reinterpret_convert(val);
+}
+
+/** Convenience for reinterpret_convert().  Enforces the type to be floating-point */
+template<typename F>
+decltype(auto) reinterpret_float_as_int(F val) {
+  static_assert(std::is_floating_point<F>::value,
+                "Must pass a floating-point type to reinterpret as integral");
+  return reinterpret_convert(val);
+}
 
 template <typename T>
 std::vector<std::tuple<T,T,T>> testDistribution(unsigned int numTrials) {
@@ -19,20 +89,29 @@ std::vector<std::tuple<T,T,T>> testDistribution(unsigned int numTrials) {
 
   std::random_device seed_generator;
   auto seed = seed_generator();
-  std::cout << "mt seed:     " << seed << std::endl;
   std::mt19937 generator32(seed);
   std::mt19937_64 generator64(seed);
+  auto generator128 = [&generator64]() {
+    unsigned __int128 val = generator64();
+    val = val << 64;
+    val += generator64();
+    return val;
+  };
 
   for (unsigned int i = 0; i < numTrials; i++) {
     T a, b, c;
     if (sizeof(T) == 4) {
-      a = my_reinterpret<T>(generator32());
-      b = my_reinterpret<T>(generator32());
-      c = my_reinterpret<T>(generator32());
+      a = reinterpret_int_as_float(static_cast<uint32_t>(generator32()));
+      b = reinterpret_int_as_float(static_cast<uint32_t>(generator32()));
+      c = reinterpret_int_as_float(static_cast<uint32_t>(generator32()));
     } else if (sizeof(T) == 8) {
-      a = my_reinterpret<T>(generator64());
-      b = my_reinterpret<T>(generator64());
-      c = my_reinterpret<T>(generator64());
+      a = reinterpret_int_as_float(static_cast<uint64_t>(generator64()));
+      b = reinterpret_int_as_float(static_cast<uint64_t>(generator64()));
+      c = reinterpret_int_as_float(static_cast<uint64_t>(generator64()));
+    } else if (sizeof(T) == 16) {
+      a = reinterpret_int_as_float(generator128());
+      b = reinterpret_int_as_float(generator128());
+      c = reinterpret_int_as_float(generator128());
     } else {
       throw std::runtime_error("Error");
     }
@@ -50,66 +129,58 @@ std::vector<std::tuple<T,T,T>> testDistribution(unsigned int numTrials) {
   return mismatchedInputs;
 }
 
-void runDistributionFloat() {
-  const unsigned int count = 1000000;//99999999;  // number of experiments
-
+void runDistributionFloat(const unsigned int count) {
   auto mismatches = testDistribution<float>(count);
+  auto printAsInt32 = [](float val) {
+    auto intval = reinterpret_float_as_int(val);
+    printf("DistributionTest:   0x%08x  %e\n", intval, val);
+  };
   for (auto& input : mismatches) {
     printf("\n");
-    printf("DistributionTest: Diverging inputs:\n");
-    printf("DistributionTest:   0x%08x  %e\n", my_reinterpret<unsigned>(std::get<0>(input)), std::get<0>(input));
-    printf("DistributionTest:   0x%08x  %e\n", my_reinterpret<unsigned>(std::get<1>(input)), std::get<1>(input));
-    printf("DistributionTest:   0x%08x  %e\n", my_reinterpret<unsigned>(std::get<2>(input)), std::get<2>(input));
-  }
-  
-  float a = my_reinterpret<float, unsigned>(0x6b8b4567);
-  float b = my_reinterpret<float, unsigned>(0x65ba0c1e);
-  float c = my_reinterpret<float, unsigned>(0x49e753d2);
-  auto truth = distributionTruth(a, b, c);
-  auto testval = distributionTest(a, b, c);
-  
-  if (truth != testval) {
-    printf("\n");
-    printf("truth      = 0x%08x\n", my_reinterpret<unsigned>(truth));
-    printf("testval    = 0x%08x\n", my_reinterpret<unsigned>(testval));
-    printf("difference = 0x%08x\n", my_reinterpret<unsigned>(truth - testval));
+    printf("DistributionTest: Diverging inputs (float):\n");
+    printAsInt32(std::get<0>(input));
+    printAsInt32(std::get<1>(input));
+    printAsInt32(std::get<2>(input));
   }
 }
 
-void runDistributionDouble() {
-  const unsigned int count = 1000000;//99999999;  // number of experiments
-
+void runDistributionDouble(const unsigned int count) {
   auto mismatches = testDistribution<double>(count);
+  auto printAsInt64 = [](double val) {
+    auto intval = reinterpret_float_as_int(val);
+    printf("DistributionTest:   0x%016lx  %e\n", intval, val);
+  };
   for (auto& input : mismatches) {
     printf("\n");
-    printf("DistributionTest: Diverging inputs:\n");
-    printf("DistributionTest:   0x%016lx  %e\n", my_reinterpret<unsigned long>(std::get<0>(input)), std::get<0>(input));
-    printf("DistributionTest:   0x%016lx  %e\n", my_reinterpret<unsigned long>(std::get<1>(input)), std::get<1>(input));
-    printf("DistributionTest:   0x%016lx  %e\n", my_reinterpret<unsigned long>(std::get<2>(input)), std::get<2>(input));
+    printf("DistributionTest: Diverging inputs (double):\n");
+    printAsInt64(std::get<0>(input));
+    printAsInt64(std::get<1>(input));
+    printAsInt64(std::get<2>(input));
   }
-  
-  auto a = my_reinterpret<double, unsigned long>(0x6b8b4567);
-  auto b = my_reinterpret<double, unsigned long>(0x65ba0c1e);
-  auto c = my_reinterpret<double, unsigned long>(0x49e753d2);
-  auto truth = distributionTruth(a, b, c);
-  auto testval = distributionTest(a, b, c);
-  
-  if (truth != testval) {
+}
+
+void runDistributionLongDouble(const unsigned int count) {
+  auto mismatches = testDistribution<long double>(count);
+  auto printAsInt80 = [](long double val) {
+    auto intval = reinterpret_float_as_int(val);
+    printf("DistributionTest:   0x%04lx%016lx  %Le\n",
+           static_cast<uint64_t>(intval >> 64),
+           static_cast<uint64_t>(intval),
+           val);
+  };
+  for (auto& input : mismatches) {
     printf("\n");
-    printf("truth      = 0x%016lx\n", my_reinterpret<unsigned long>(truth));
-    printf("testval    = 0x%016lx\n", my_reinterpret<unsigned long>(testval));
-    printf("difference = 0x%016lx\n", my_reinterpret<unsigned long>(truth - testval));
+    printf("DistributionTest: Diverging inputs (long double):\n");
+    printAsInt80(std::get<0>(input));
+    printAsInt80(std::get<1>(input));
+    printAsInt80(std::get<2>(input));
   }
 }
 
 int main(void) {
-  //std::cout << "float min:   " << std::numeric_limits<float>::min() << std::endl;
-  //std::cout << "float max:   " << std::numeric_limits<float>::max() << std::endl;
-  //std::cout << "double min:  " << std::numeric_limits<double>::min() << std::endl;
-  //std::cout << "double max:  " << std::numeric_limits<double>::max() << std::endl;
-
-  //runDistributionFloat();
-  runDistributionDouble();
+  runDistributionFloat(100);
+  runDistributionDouble(100);
+  runDistributionLongDouble(20);
 
   return 0;
 }
