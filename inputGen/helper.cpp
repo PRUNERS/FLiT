3ee387385b89937b5cd63eb240978de80c5041f1
Michael Bentley <mikebentley15@gmail.com>
2016-09-12 17:05:14 -0600

    inputGen: implement argument parsing with many options
    The options implemented are:
    --float | --double || --long-double || --all-types
    --max-tries
    --output
    --divergent-input-count
    --test
    --all-tests

--- inputGen/helper.cpp
+++ inputGen/helper.cpp
@@ -2,23 +2,59 @@
 
 #include "QFPHelpers.hpp"
 
+#include <iostream>
+#include <iomanip>
+
+/// RAII class for restoring iostream formats
+class FmtRestore {
+public:
+  FmtRestore(std::ios& stream) : _stream(stream), _state(nullptr) {
+    _state.copyfmt(_stream);
+  }
+  ~FmtRestore() { _stream.copyfmt(_state); }
+private:
+  std::ios& _stream;
+  std::ios  _state;
+};
+
 void printTestVal(const std::string &funcName, float val) {
+  FmtRestore restorer(std::cout);
+  Q_UNUSED(restorer);
+
   auto intval = QFPHelpers::as_int(val);
-  printf("%s:     0x%08x  %e\n", funcName.c_str(), intval, val);
+  std::cout << funcName << ":     0x"
+            << std::hex << std::setw(8) << std::setfill('0') << intval
+            << "  "
+            << std::scientific << val
+            << std::endl;
 }
 
 void printTestVal(const std::string &funcName, double val) {
+  FmtRestore restorer(std::cout);
+  Q_UNUSED(restorer);
+
   auto intval = QFPHelpers::as_int(val);
-  printf("%s:     0x%016lx  %e\n", funcName.c_str(), intval, val);
+  std::cout << funcName << ":     0x"
+            << std::hex << std::setw(16) << std::setfill('0') << intval
+            << "  "
+            << std::scientific << val
+            << std::endl;
 }
 
 void printTestVal(const std::string &funcName, long double val) {
+  FmtRestore restorer(std::cout);
+  Q_UNUSED(restorer);
+
   auto intval = QFPHelpers::as_int(val);
-  printf("%s:     0x%04lx%016lx  %Le\n",
-         funcName.c_str(),
-         static_cast<uint64_t>((intval >> 64)) & 0xFFFFL,
-         static_cast<uint64_t>(intval),
-         val);
+  uint64_t lhalf = static_cast<uint64_t>((intval >> 64)) & 0xFFFFL;
+  uint64_t rhalf = static_cast<uint64_t>(intval);
+
+  std::cout << funcName << ":     0x"
+            << std::hex << std::setw(4) << std::setfill('0') << lhalf
+            << std::hex << std::setw(16) << std::setfill('0') << rhalf
+            << "  "
+            << std::scientific << val
+            << std::endl;
 }
 
 namespace {
