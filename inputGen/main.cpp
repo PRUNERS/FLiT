426c0163ac5c1ebf5c8c3cf69931e688a7f45378
Michael Bentley <mikebentley15@gmail.com>
2016-09-12 17:05:14 -0600

    inputGen: implement argument parsing with many options
    The options implemented are:
    --float | --double || --long-double || --all-types
    --max-tries
    --output
    --divergent-input-count
    --test
    --all-tests

--- inputGen/main.cpp
+++ inputGen/main.cpp
@@ -4,7 +4,10 @@
 //#include "testbed.h"
 
 #include <cmath>
+#include <fstream>
 #include <iostream>
+#include <list>
+#include <memory>
 #include <random>
 #include <tuple>
 #include <type_traits>
@@ -94,22 +97,188 @@ std::vector<std::string> getTestNames() {
   return retval;
 }
 
+/// An RAII class for replacing a stream buffer and restoring in destruction
+class BufSwap {
+public:
+  BufSwap(std::ostream& stream, std::streambuf* newbuf)
+    : _stream(stream), _oldbuf(stream.rdbuf()), _newbuf(newbuf)
+  { _stream.rdbuf(_newbuf); }
 
-int main(void) {
-  for (auto testname : getTestNames()) {
-    runTest<float>(testname, 10, 1000);
+  ~BufSwap() { _stream.rdbuf(_oldbuf); }
+
+private:
+  std::ostream& _stream;
+  std::streambuf* _oldbuf;
+  std::streambuf* _newbuf;
+};
+
+/// All options from command-line parsing are stored here
+struct ProgramArguments {
+  enum class PrecisionType {
+    Float,
+    Double,
+    LongDouble,
+    All,
+  };
+  PrecisionType type = PrecisionType::Float;
+  uint maxTries = 1000;
+  bool outputToFile = false;
+  std::string outFilename;
+  uint divergentCount = 10;
+  std::vector<std::string> tests;
+};
+ProgramArguments parseArgs(int argCount, char* argList[]);
+
+int main(int argCount, char* argList[]) {
+  auto parsedArgs = parseArgs(argCount, argList);
+
+  auto runner = runAllPrecisions;
+  switch(parsedArgs.type) {
+    case ProgramArguments::PrecisionType::Float:
+      runner = runTest<float>;
+      break;
+    case ProgramArguments::PrecisionType::Double:
+      runner = runTest<double>;
+      break;
+    case ProgramArguments::PrecisionType::LongDouble:
+      runner = runTest<long double>;
+      break;
+    case ProgramArguments::PrecisionType::All:
+      runner = runAllPrecisions;
+      break;
   }
-  // Sanity check: should not find any differences no matter the optimizations
-  //runAllPrecisions("DoMatrixMultSanity", 10, 1000);
 
-  //runTest<float>("DistributivityOfMultiplication", 10, 100000);
-  //runTest<double>("DistributivityOfMultiplication", 10, 100000);
-  //runTest<long double>("DistributivityOfMultiplication", 10, 100000);
-  //runAllPrecisions("DistributivityOfMultiplication", 10, 10000);
+  // Set the console output to a file if specified
+  std::unique_ptr<std::ofstream> out(nullptr);
+  std::unique_ptr<BufSwap> swapper(nullptr);
+  if (parsedArgs.outputToFile) {
+    out.reset(new std::ofstream(parsedArgs.outFilename));
+    swapper.reset(new BufSwap(std::cout, out->rdbuf()));
+  }
 
-  //runAllPrecisions("DoHariGSBasic", 10, 100000);
-  //QFPHelpers::info_stream.show();
+  for (auto test : parsedArgs.tests) {
+    runner(test, parsedArgs.divergentCount, parsedArgs.maxTries);
+  }
 
   return 0;
 }
 
+ProgramArguments parseArgs(int argCount, char* argList[]) {
+  const char* USAGE_INFO = "\n"
+    "Usage:\n"
+    "  inputGen [-h]\n"
+    "  inputGen --list-tests\n"
+    "  inputGen [-f|-d|-e|-a] [-m MAX_TRIES] [-o FILENAME] [-i NUM_DIVERGENT_INPUTS]\n"
+    "           (--test TEST_NAME ... | --all-tests)\n"
+    "\n"
+    "Description:\n"
+    "  Runs the particular tests under the optimization given in compilation.  The\n"
+    "  test input is randomly generated floating point numbers.  It runs until the\n"
+    "  number of desired divergences are discovered between the unoptimized code\n"
+    "  and the optimized code.\n"
+    "\n"
+    "Required Arguments:\n"
+    "  One of the following must be used since we need at least one test to run.\n"
+    "  The --test option can be specified more than once to specify more than one\n"
+    "  test.\n"
+    "\n"
+    "  --test TEST_NAME\n"
+    "      The test to run.  For a list of choices, see --list-tests.\n"
+    "\n"
+    "  --all-tests\n"
+    "      Run all tests that are returned from --list-tests.\n"
+    "\n"
+    "Optional Arguments:\n"
+    "\n"
+    "  -f, --float\n"
+    "      Run each test only with float precision.  This is the default.\n"
+    "\n"
+    "  -d, --double\n"
+    "      Run each test only with double precision.\n"
+    "\n"
+    "  -e, --long-double\n"
+    "      Run each test only with extended precision, also called long double.\n"
+    "\n"
+    "  -a, --all-types\n"
+    "      Run each test with all precisions.\n"
+    "\n"
+    "  -i NUM_DIVERGENT_INPUTS, --divergent-input-count NUM_DIVERGEN_INPUTS\n"
+    "      How many divergent inputs to attempt to identify and report.  The\n"
+    "      default value is 10.\n"
+    "\n"
+    "  -m MAX_TRIES, --max-tries MAX_TRIES\n"
+    "      How many tries before giving up on search for the specified numer of\n"
+    "      divergent inputs.  The default value is 1,000.\n"
+    "\n"
+    "  -o FILENAME, --output FILENAME\n"
+    "      Where to output the report.  By default it outputs to stdout.\n"
+    "\n"
+    ;
+
+  std::vector<std::string> args;
+  for (decltype(argCount) i = 1; i < argCount; i++) {
+    args.push_back(argList[i]);
+  }
+
+  ProgramArguments parsedArgs;
+
+  auto is_in = [](std::vector<std::string> vec, std::string val) {
+    return std::any_of(vec.begin(), vec.end(),
+        [&val](std::string it) { return it == val; });
+  };
+
+  // Look for help
+  if (is_in(args, "-h") || is_in(args, "--help")) {
+    std::cout << USAGE_INFO;
+    std::exit(0);
+  }
+
+  // Look for --list-tests
+  if (is_in(args, "--list-tests")) {
+    for (auto test : getTestNames()) {
+      std::cout << test << std::endl;
+    }
+    std::exit(0);
+  }
+
+  // Handle any other argument
+  for (decltype(args.size()) i = 0; i < args.size(); i++) {
+    auto& arg = args[i];
+    if (arg == "-f" || arg == "--float") {
+      parsedArgs.type = ProgramArguments::PrecisionType::Float;
+    } else if (arg == "-d" || arg == "--double") {
+      parsedArgs.type = ProgramArguments::PrecisionType::Double;
+    } else if (arg == "-e" || arg == "--long-double") {
+      parsedArgs.type = ProgramArguments::PrecisionType::LongDouble;
+    } else if (arg == "-a" || arg == "--all-types") {
+      parsedArgs.type = ProgramArguments::PrecisionType::All;
+    } else if (arg == "-m" || arg == "--max-tries") {
+      auto& nextArg = args[++i];
+      parsedArgs.maxTries = static_cast<uint>(std::stoul(nextArg));
+    } else if (arg == "-o" || arg == "--output") {
+      auto& nextArg = args[++i];
+      parsedArgs.outputToFile = true;
+      parsedArgs.outFilename = nextArg;
+    } else if (arg == "-i" || arg == "--divergent-input-count") {
+      auto& nextArg = args[++i];
+      parsedArgs.divergentCount = static_cast<uint>(std::stoul(nextArg));
+    } else if (arg == "--test") {
+      auto& nextArg = args[++i];
+      parsedArgs.tests.push_back(nextArg);
+    } else if (arg == "--all-tests") {
+      auto allTests = getTestNames();
+      parsedArgs.tests.insert(parsedArgs.tests.end(), allTests.begin(), allTests.end());
+    } else {
+      std::cerr << "Error: unrecognized argument: " << arg << std::endl;
+      std::cerr << "  Use the --help option for more information" << std::endl;
+    }
+  }
+
+  if (parsedArgs.tests.size() == 0) {
+    std::cerr << "Error: you need to specify at least one test" << std::endl;
+    std::cerr << "  Use the --help option for more information" << std::endl;
+    std::exit(1);
+  }
+
+  return parsedArgs;
+}
