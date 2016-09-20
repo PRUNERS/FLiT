3054f1ad666183f27a9a3dcec34fcf6648997fa1
Michael Bentley <mikebentley15@gmail.com>
2016-09-19 21:41:54 -0600

    Get it compiling and working
    The triangle tests are still not being run in CUDA

--- qfpc/tests/Triangle.hpp
+++ qfpc/tests/Triangle.hpp
@@ -7,14 +7,50 @@
 #include "testBase.hpp"
 #include "QFPHelpers.hpp"
 #include "CUHelpers.hpp"
-#include "cudaTests.hpp"
 
 using namespace CUHelpers;
 using namespace std::placeholders;
 
+template <typename T, typename Klass>
+GLOBAL
+void
+TriangleKernel(const QFPTest::CuTestInput<T>* tiList, QFPTest::CudaResultElement* results){
+#ifdef __CUDA__
+  auto idx = blockIdx.x * blockDim.x + threadIdx.x;
+#else
+  auto idx = 0;
+#endif
+  
+  auto ti = tiList[idx];
+  T maxval = ti.vals[0];
+  // start as a right triangle
+  T a = maxval;
+  T b = maxval;
+  T c = maxval * sqrt(2.0);
+  const T delta = maxval / (T)ti.iters;
+
+  // 1/2 b*h = A
+  // all perturbations will have the same base and height (plus some FP noise)
+  const T checkVal = 0.5 * b * a;
+
+  long double score = 0;
+
+  for(T pos = 1; pos <= a; pos += delta){
+    auto crit = getArea(a,b,c);
+    b = sqrt(pow(pos, 2) +
+             pow(maxval, 2));
+    c = sqrt(pow(a - pos, 2) +
+             pow(maxval, 2));
+    score += abs(crit - checkVal);
+  }
+  results[idx].s1 = score;
+  results[idx].s2 = 0.0;
+}
+
 template <typename T>
 class Triangle: public QFPTest::TestBase<T> {
 protected:
+  HOST_DEVICE Triangle() = default;
   Triangle(std::string id) : QFPTest::TestBase<T>(std::move(id)) {}
 
 public:
@@ -27,8 +63,13 @@ public:
   }
 
 protected:
+  HOST_DEVICE
   virtual T getArea(const T a, const T b, const T c) = 0;
 
+  // TODO: figure out how to get this one to work.
+  virtual QFPTest::KernelFunction<T>* getKernel() { return nullptr; } //TriangleKernel<T, decltype(*this)>; }
+
+  virtual
   QFPTest::ResultType::mapped_type run_impl(const QFPTest::TestInput<T>& ti) {
     T maxval = ti.vals[0];
     // start as a right triangle
@@ -47,7 +88,6 @@ protected:
 
     for(T pos = 1; pos <= a; pos += delta){
       auto crit = getArea(a,b,c);
-      // crit = getArea(a,b,c);
       b = std::sqrt(std::pow(pos, 2) +
                     std::pow(maxval, 2));
       c = std::sqrt(std::pow(a - pos, 2) +
