de5dacc8850384ecdbb8a56d35a3e39bacae12d3
Geof23 <fredericflintstone@gmail.com>
2016-11-30 15:19:40 -0700

    this commit has major changes to live up to the branch name -- unified_script.  The main process now takes 3 host types: Master, Worker and DB

---
+++ hostfile.py
@@ -0,0 +1,9 @@
+#this python file contains the DB_HOST and RUN_HOSTS objects
+
+#DB_HOST: (user, fqdn)
+DB_HOST = ('sawaya', bihexal.cs.utah.edu)
+
+#the format of RUN_HOSTS tuples: (user, fqdn, procs, [CUDA only: (True | False)])
+RUN_HOSTS = (('u0422778', 'kp328', 56, False),('u0422778', 'kp298', 1, True))
+
+ 
---
+++ prepDBHost.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+#this script prepares the db host by making a place to
+#push the output files to so that the import processes may
+#be executed upon them
+
+import tempfile, os, glob
+from subprocess import check_output
+
+COLL_DIR = 'flit_data'
+TAR = check_output('which tar', shell=True, universal_newlines=True)[:-1]
+GZIP = check_output('which gzip', shell=True, universal_newlines=True)[:-1]
+
+try:
+    os.mkdir(COLL_DIR)
+except FileExistsError:
+    if len(glob.glob(COLL_DIR + '/*')) > 0:
+        os.chdir(COLL_DIR)
+        f = tempfile.mkstemp('.tar', dir='..', prefix=COLL_DIR + '_')
+        stdot = check_output(TAR + ' uf ' + f[1] + ' *', shell=True,
+                             universal_newlines=True)
+        stdoe = check_output('rm *', shell=True,
+                             universal_newlines=True)
+        stdog = check_output(GZIP + ' ' + f[1], shell=True,
+                             universal_newlines=True)
+        print('created ' + f[1])
+    else:
+        print(COLL_DIR + ' was empty')
+    exit(0)
+print('made directory ' + COLL_DIR)
+exit(0)
