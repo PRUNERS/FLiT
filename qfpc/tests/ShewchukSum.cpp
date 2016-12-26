1b66887925c281903061d490992fbca5cd1ee33c
Michael Bentley <mikebentley15@gmail.com>
2016-12-26 14:36:50 -0700

    Fix Shewchuk test

--- qfpc/tests/ShewchukSum.cpp
+++ qfpc/tests/ShewchukSum.cpp
@@ -8,31 +8,36 @@
 #include <vector>
 
 template <typename T>
-class ShewChuck {
+class Shewchuk {
 public:
-  ShewChuck() : m_count(0) {}
+  Shewchuk() : m_count(0) {}
 
   void count() { return m_count; }
 
   void add(const T next) {
     T x = next;
-    size_t partials_len = m_partials.length();
-    for (size_t j = 0; j < partials_len; j++) {
-      T y = m_partials[j];
+    size_t i = 0;
+    for (T& y : m_partials) {
       if (std::abs(x) < std::abs(y)) {
         const T temp = x;
         x = y;
         y = temp;
       }
       const T hi = x + y;
-      //const T lo = y - ((volatile T)hi - x);
       const T lo = y - (hi - x);
       if (lo != 0.0) {
-        m_partials.push_back(lo);
+        m_partials[i] = lo;
+        i += 1;
       }
       x = hi;
     }
-    m_partials.push_back(x);
+
+    if (m_partials.size() <= i) {
+      m_partials.push_back(x);
+    } else {
+      m_partials[i] = x;
+      m_partials.resize(i+1);
+    }
     m_count += 1;
   }
 
@@ -54,16 +59,16 @@ private:
 };
 
 template <typename T>
-class ShewChuckSum : public QFPTest::TestBase<T> {
+class ShewchukSum : public QFPTest::TestBase<T> {
 public:
-  ShewChuckSum(std::string id) : QFPTest::TestBase<T>(std::move(id)) {}
+  ShewchukSum(std::string id) : QFPTest::TestBase<T>(std::move(id)) {}
   
   virtual size_t getInputsPerRun() { return 1000; }
   virtual QFPTest::TestInput<T> getDefaultInput();
 
 protected:
   virtual QFPTest::ResultType::mapped_type run_impl(const QFPTest::TestInput<T>& ti) {
-    ShewChuck<T> chuck;
+    Shewchuk<T> chuck;
     for (auto val : ti.vals) {
       chuck.add(val);
     }
