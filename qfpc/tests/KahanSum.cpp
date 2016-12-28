984df178857722752480b03ff910db96abd8c528
Michael Bentley <mikebentley15@gmail.com>
2016-12-27 17:14:14 -0700

    Get Kahan and Shewchuk working with framework

--- qfpc/tests/KahanSum.cpp
+++ qfpc/tests/KahanSum.cpp
@@ -4,6 +4,8 @@
 #include "QFPHelpers.hpp"
 #include "CUHelpers.hpp"
 
+#include <iomanip>
+#include <iostream>
 #include <string>
 
 template <typename T>
@@ -11,19 +13,58 @@ class KahanSum : public QFPTest::TestBase<T> {
 public:
   KahanSum(std::string id) : QFPTest::TestBase<T>(std::move(id)) {}
 
-  virtual size_t getInputsPerRun() { return 1000; }
+  virtual size_t getInputsPerRun() { return 3; } //10000; }
   virtual QFPTest::TestInput<T> getDefaultInput();
 
 protected:
   virtual QFPTest::ResultType::mapped_type run_impl(const QFPTest::TestInput<T>& ti) {
     Kahan<T> kahan;
+    T naive = 0.0;
     for (auto val : ti.vals) {
       kahan.add(val);
+      naive += val;
+      QFPHelpers::info_stream
+      //std::cout
+        //<< std::setiosflags(std::ios::scientific)
+        << std::setw(7)
+        << std::setprecision(7)
+        << id << ": + " << val
+        << " = " << kahan.sum() << " or " << naive
+        << std::endl;
     }
-    return {kahan.sum(), 0.0};
+    QFPHelpers::info_stream << id << ": naive sum = " << naive << std::endl;
+    QFPHelpers::info_stream << id << ": kahan sum = " << kahan.sum() << std::endl;
+    QFPHelpers::info_stream
+      << id << ": sum = " << kahan.sum() << " or " << naive << std::endl;
+    return {kahan.sum(), naive};
   }
 
 protected:
   using QFPTest::TestBase<T>::id;
 };
 
+namespace {
+template<typename T> std::vector<T> getToRepeat();
+
+//template<> std::vector<float> getToRepeat() { return { 1.0, 1.0e8, -1.0e8 }; }
+template<> std::vector<float> getToRepeat() { return { 1.0e4, 3.14159, 2.71828 }; }
+template<> std::vector<double> getToRepeat() { return { 1.0, 1.0e100, 1.0, -1.0e100 }; }
+template<> std::vector<long double> getToRepeat() { return { 1.0, 1.0e200, 1.0, -1.0e200 }; }
+}
+
+template <typename T>
+QFPTest::TestInput<T> KahanSum<T>::getDefaultInput() {
+  QFPTest::TestInput<T> ti;
+  auto dim = getInputsPerRun();
+  ti.highestDim = dim;
+  ti.vals = std::vector<T>(dim);
+  auto toRepeat = getToRepeat<T>();
+  for (decltype(dim) i = 0, j = 0;
+       i < dim;
+       i++, j = (j+1) % toRepeat.size()) {
+    ti.vals[i] = toRepeat[j];
+  }
+  return ti;
+}
+
+REGISTER_TYPE(KahanSum)
--- qfpc/tests/ShewchukSum.cpp
+++ qfpc/tests/ShewchukSum.cpp
@@ -1,62 +1,12 @@
-#include "Kahan.h"
+#include "Shewchuk.h"
 
 #include "testBase.hpp"
 #include "QFPHelpers.hpp"
 #include "CUHelpers.hpp"
 
+#include <iomanip>
 #include <string>
 #include <vector>
-
-template <typename T>
-class Shewchuk {
-public:
-  Shewchuk() : m_count(0) {}
-
-  void count() { return m_count; }
-
-  void add(const T next) {
-    T x = next;
-    size_t i = 0;
-    for (T& y : m_partials) {
-      if (std::abs(x) < std::abs(y)) {
-        const T temp = x;
-        x = y;
-        y = temp;
-      }
-      const T hi = x + y;
-      const T lo = y - (hi - x);
-      if (lo != 0.0) {
-        m_partials[i] = lo;
-        i += 1;
-      }
-      x = hi;
-    }
-
-    if (m_partials.size() <= i) {
-      m_partials.push_back(x);
-    } else {
-      m_partials[i] = x;
-      m_partials.resize(i+1);
-    }
-    m_count += 1;
-  }
-
-  T sum() {
-    Kahan<T> summer;
-    for (auto val : m_partials) {
-      summer.add(val);
-    }
-    return summer.sum();
-  }
-
-  T average() {
-    return sum() / m_count;
-  }
-
-private:
-  std::vector<T> m_partials;
-  size_t m_count;
-};
 
 template <typename T>
 class ShewchukSum : public QFPTest::TestBase<T> {
@@ -68,13 +18,56 @@ public:
 
 protected:
   virtual QFPTest::ResultType::mapped_type run_impl(const QFPTest::TestInput<T>& ti) {
-    Shewchuk<T> chuck;
+    Shewchuk<T> chuk;
+    T naive = 0.0;
     for (auto val : ti.vals) {
-      chuck.add(val);
+      chuk.add(val);
+      naive += val;
+      //QFPHelpers::info_stream
+      //  << std::setw(7)
+      //  << std::setprecision(7)
+      //  << id << ": + " << val
+      //  << " = " << chuk.sum() << " or " << naive
+      //  << std::endl;
+			QFPHelpers::info_stream
+				<< id << ":   partials now: (" << chuk.partials().size() << ") ";
+      for (auto p : chuk.partials()) {
+        QFPHelpers::info_stream << " " << p;
+      }
+      QFPHelpers::info_stream << std::endl;
     }
-    return {chuck.sum(), chuck.average()};
+    T sum = chuk.sum();
+    QFPHelpers::info_stream << id << ": naive sum    = " << naive << std::endl;
+    QFPHelpers::info_stream << id << ": shewchuk sum = " << sum << std::endl;
+    QFPHelpers::info_stream << id << ": shewchuk partials = " << chuk.partials().size() << std::endl;
+    return {sum, chuk.sum2()};
   }
 
 protected:
   using QFPTest::TestBase<T>::id;
 };
+
+namespace {
+template<typename T> std::vector<T> getToRepeat();
+
+template<> std::vector<float> getToRepeat() { return { 1.0, 1.0e8, 1.0, -1.0e8 }; }
+template<> std::vector<double> getToRepeat() { return { 1.0, 1.0e100, 1.0, -1.0e100 }; }
+template<> std::vector<long double> getToRepeat() { return { 1.0, 1.0e200, 1.0, -1.0e200 }; }
+}
+
+template <typename T>
+QFPTest::TestInput<T> ShewchukSum<T>::getDefaultInput() {
+  QFPTest::TestInput<T> ti;
+  auto dim = getInputsPerRun();
+  ti.highestDim = dim;
+  ti.vals = std::vector<T>(dim);
+  auto toRepeat = getToRepeat<T>();
+  for (decltype(dim) i = 0, j = 0;
+       i < dim;
+       i++, j = (j+1) % toRepeat.size()) {
+    ti.vals[i] = toRepeat[j];
+  }
+  return ti;
+}
+
+REGISTER_TYPE(ShewchukSum)
