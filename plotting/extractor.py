891681818a6f85fd63668af7bc6bfce4833e0820
Michael Bentley <mikebentley15@gmail.com>
2017-01-17 00:10:09 -0700

    extractor.py: get working with psycopg2 too

--- plotting/extractor.py
+++ plotting/extractor.py
@@ -3,8 +3,35 @@
 
 import argparse
 import csv
-import pg
 import sys
+
+try:
+    import pg
+    using_pg = True
+except ImportError:
+    import psycopg2
+    using_pg = False
+
+def connect_to_database():
+    'Returns a database handle for the framework being used'
+    dbname = 'qfp'
+    host = 'localhost'
+    user = 'qfp'
+    passwd = 'qfp123'
+    if using_pg:
+        return pg.DB(dbname=dbname, host=host, user=user, passwd=passwd)
+    else:
+        conn_string = ''
+        conn = psycopg2.connect(conn_string)
+        return conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+
+def run_query(handle, query, *args, **kwargs):
+    'Runs the query and returns the result as a list of dictionaries'
+    if using_pg:
+        return handle.query(query, *args, **kwargs).dictresults()
+    else:
+        handle.execute(query, *args, **kwargs)
+        return handle.fetchall()
 
 def query_results_to_file(filename, query):
     'Writes results from a PyGresQL query object to a csv file.'
@@ -23,10 +50,10 @@ def main(arguments):
                         help='A run to extract to csv.  Saved as run-N.csv')
     args = parser.parse_args(args=arguments)
 
-    pgdb = pg.DB(dbname='qfp', host='localhost', user='qfp', passwd='qfp123')
+    db = connect_to_database()
     runs = args.runs
     if len(runs) == 0:
-        query_runs = pgdb.query('select index from runs;')
+        query_runs = run_query(db, 'select index from runs;')
         idx = query_runs.dictresult()[-1]['index']
         runs.append(idx)
 
@@ -34,7 +61,7 @@ def main(arguments):
         filename = 'run-{0:02}.csv'.format(run)
         sys.stdout.write('writing ' + filename + ' ...')
         sys.stdout.flush()
-        query_tests = pgdb.query('select * from tests where run = $1;', run)
+        query_tests = run_query(db, 'select * from tests where run = $1;', run)
         query_results_to_file(filename, query_tests)
         sys.stdout.write(' done\n')
         sys.stdout.flush()
