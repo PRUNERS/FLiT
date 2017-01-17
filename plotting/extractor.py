6b89ab704569bfa8bdd1732866f0bf36d9594042
Michael Bentley <mikebentley15@gmail.com>
2017-01-17 00:39:28 -0700

    extractor.py: finish fixing the psycopg2 stuff

--- plotting/extractor.py
+++ plotting/extractor.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 'Extracts test run data into csv files'
 
+from __future__ import print_function
+
 import argparse
 import csv
 import sys
@@ -33,12 +35,13 @@ def run_query(handle, query, *args, **kwargs):
         return handle.query(query, *args, **kwargs).dictresults()
     else:
         handle.execute(query, *args, **kwargs)
-        return handle.fetchall()
+        return [dict(x) for x in handle.fetchall()]
 
 def query_results_to_file(filename, query):
     'Writes results from a PyGresQL query object to a csv file.'
+    print(query[0])
     with open(filename, 'w') as outfile:
-        writer = csv.DictWriter(outfile, query[0].keys())
+        writer = csv.DictWriter(outfile, sorted(query[0].keys()))
         writer.writeheader()
         writer.writerows(query)
 
@@ -63,7 +66,7 @@ def main(arguments):
         filename = 'run-{0:02}.csv'.format(run)
         sys.stdout.write('writing ' + filename + ' ...')
         sys.stdout.flush()
-        query_tests = run_query(db, 'select * from tests where run = $1;', run)
+        query_tests = run_query(db, 'select * from tests where run = \'{0}\';'.format(run))
         query_results_to_file(filename, query_tests)
         sys.stdout.write(' done\n')
         sys.stdout.flush()
