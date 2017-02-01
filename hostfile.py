5244eb14c325a6f0b115db82fc66c29e58fc6208
Geof23 <fredericflintstone@gmail.com>
2017-01-31 20:29:30 -0700

    Trying to get the build system complete and consistent. Having isssues on Bihexal -- clang is always getting the includes wrong (where it relies on the gcc-toolchain).

--- hostfile.py
+++ hostfile.py
@@ -4,6 +4,10 @@
 DB_HOST = ('sawaya', 'bihexal.cs.utah.edu')
 
 #the format of RUN_HOSTS tuples: (user, fqdn, procs, [CUDA only: (True | False)])
-RUN_HOSTS = (('u0422778', 'kp334', 56, False),('u0422778', 'kp297', 1, True))
+RUN_HOSTS = (('u0422778', 'kingspeak.chpc.utah.edu', 56, False,
+              kingspeak_cpu_startup),
+             ('u0422778', 'kingspeak.chpc.utah.edu', 1, True,
+              kingspeak_gpu_startup),
+             ('sawaya', 'ms0345.utah.cloudlab.us', 8, False, ''))
 
  
