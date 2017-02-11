99b08408b104334ca95b7ffcaa1921e9cae5a423
Michael Bentley <mikebentley15@gmail.com>
2017-02-10 18:52:35 -0700

    Finish the schmoo, but something is wrong

--- plotting/schmoo.py
+++ plotting/schmoo.py
@@ -1,6 +1,4 @@
-#!/usr/bin/env python
-
-from __future__ import print_function
+#!/usr/bin/env python3
 
 import argparse
 import sys
@@ -8,6 +6,8 @@ import sys
 import extractor
 
 import numpy as np
+
+from collections import OrderedDict
 
 # note: the /etc/matplotlibrc (on Ubuntu 16)  file has to be configured to use
 # the 'Agg' backend (see the file for details).  This is so it will work
@@ -62,15 +62,60 @@ def main(arguments):
         ''')
     parser.add_argument('-r', '--run', metavar='N', type=int, default=-1,
                         help='Which run to use.  Default is the latest run.')
-    parser.add_argument('-p', '--precision', choices=['f', 'd', 'e'], default='f',
-                        help='Which precision to use.  Default is "f" for float')
-    parser.add_argument('-c', '--compiler', type=str, default='g++',
-                        help='Which compiler to use.  Default is "g++"')
-    parser.add_argument('-o', '--output', type=str, default='output.png',
+    parser.add_argument('-p', '--precision', default='f',
+                        help='Which precision to use.  Default is "f" for float.'
+                             '  Note: if you want to use more than one, list them'
+                             '  separated by a comma')
+    parser.add_argument('-c', '--compiler', default='g++',
+                        help='Which compiler to use.  Default is "g++".'
+                             '  Note: if you want to use more than one, list them'
+                             '  separated by a comma')
+    parser.add_argument('-H', '--host',
+                        help='Which host to use.  Must specify a value here.'
+                             '  Note: if you want to use more than one, list them'
+                             '  separated by a comma')
+    parser.add_argument('-O', '--optl', default='-O0',
+                        help='Which optimization levels to include.  Default is -O0.'
+                             '  Note: if you want to use more than one, list them'
+                             '  separated by a comma')
+    parser.add_argument('-o', '--output', default='output.png',
                         help='Where to output resultant plot.  Default is output.png')
     #parser.add_argument('-l', '--list', action='store_true',
     #                    help='List the avilable runs for download')
     args = parser.parse_args(args=arguments)
+    
+    hosts = args.host.split(',')
+    precisions = args.precision.split(',')
+    compilers = args.compiler.split(',')
+    optimization_levels = args.optl.split(',')
+    
+    def print_list(name, values):
+        print(name + ':')
+        if len(values) > 0:
+            print('  ' + '\n  '.join(values))
+    print('run: ', args.run)
+    print_list('hosts', hosts)
+    print_list('precisions', precisions)
+    print_list('compilers', compilers)
+    print_list('optimization levels', optimization_levels)
+    
+    def gen_query_sub(field_name, values):
+        '''
+        Creates a portion of a query for one of the many values to be matched
+        '''
+        sub = ''
+        if len(values) > 0:
+            sub = ' and (' + field_name + " = '"
+            sub += "' or compiler = '".join(values)
+            sub += "')"
+        return sub
+
+    host_str = gen_query_sub('host', hosts)
+    prec_str = gen_query_sub('precision', precisions)
+    comp_str = gen_query_sub('compiler', compilers)
+    optl_str = gen_query_sub('optl', optimization_levels)
+    
+        
 
     db = extractor.connect_to_database()
     
@@ -90,30 +135,62 @@ def main(arguments):
             "    and t1.run = run" +
             "    and t1.compiler = compiler" +
             "  )" +
-            " and run = {0}".format(args.run) +
-            " and precision = '{0}'".format(args.precision) +
-            " and compiler = '{0}'".format(args.compiler) +
+            "  and run = {0}".format(args.run) +
+            "  " + prec_str +
+            "  " + optl_str +
+            "  " + comp_str +
+            "  " + host_str +
             " order by name")
     tests = extractor.run_query(db, quer)
-
-    tests_str = ""
-    if len(tests) > 0:
-        tests_str = " and (name = '"
-        for t in tests:
-            tests_str += t['name'] + "' or name = '"
-        tests_str = tests_str[:-12] + ")"
+    test_str = gen_query_sub('name', [x['name'] for x in tests])
 
     quer = ("select" +
-            " distinct switches, compiler, precision" +
+            " distinct" +
+            "  switches," +
+            "  compiler," +
+            "  optl," +
+            "  precision," +
+            "  host" +
             " from tests"
             " where" +
             "  run = {0}".format(args.run) +
-            "  and precision = '{0}'".format(args.precision) +
-            "  and compiler = '{0}'".format(args.compiler) +
-            "  " + tests_str +
-            " order by compiler, switches")
+            "  " + prec_str +
+            "  " + optl_str +
+            "  " + comp_str +
+            "  " + host_str +
+            "  " + test_str +
+            " UNION " +
+            "select" +
+            " distinct" +
+            "  switches," +
+            "  compiler," +
+            "  optl," +
+            "  precision," +
+            "  host" +
+            " from tests where" +
+            "  run = {0}".format(args.run) +
+            "  " + prec_str +
+            "  " + comp_str +
+            "  " + host_str +
+            "  " + test_str +
+            "  and optl = '-O0'" +
+            "  and switches = ''" +
+            " order by compiler, optl, switches")
     x_axis = extractor.run_query(db, quer)
     xa_count = len(x_axis)
+
+    quer = ("select" +
+            " distinct" +
+            "  name" +
+            " from tests" +
+            " where" +
+            "  run = {0}".format(args.run) +
+            "  " + prec_str +
+            "  " + test_str +
+            "  " + comp_str +
+            " order by name")
+    y_axis = extractor.run_query(db, quer)
+    ya_count = len(y_axis)
 
     x_ticks = []
     y_ticks = []
@@ -122,68 +199,66 @@ def main(arguments):
     x_count = 0
     y_count = 0
 
-    ret_val = ""
-
     for x in x_axis:
-        x_ticks.append(x['switches'])
-    scores = []
-    for t in tests:
+        x_ticks.append(x['switches'] + ' ' + x['optl'])
+    for t in y_axis:
         y_ticks.append(t['name'])
         y_count += 1
-        del scores[:]
-        quers = ("select distinct score0, switches, " +
-                 "compiler, optl from " +
-                 " tests where run = " + str(args.run) +
-                 " and name = '" + t['name'] + "'" + prec_str + comp_str +
-                 " and optl = '-O0'" +
+        quers = ("select" +
+                 " distinct" +
+                 "  score0," +
+                 "  switches," +
+                 "  compiler," +
+                 "  optl," +
+                 "  host" +
+                 " from tests where" +
+                 "  run = {0}".format(args.run) +
+                 "  and name = '{0}'".format(t['name']) +
+                 "  " + prec_str +
+                 "  " + comp_str +
+                 "  " + host_str +
+                 "  and optl = '-O0'" +
+                 "  and switches = ''" +
+                 " UNION " +
+                 "select" +
+                 " distinct" +
+                 "  score0," +
+                 "  switches," +
+                 "  compiler," +
+                 "  optl," +
+                 "  host" +
+                 " from tests where" +
+                 "  run = {0}".format(args.run) +
+                 "  and name = '{0}'".format(t['name']) +
+                 "  " + prec_str +
+                 "  " + comp_str +
+                 "  " + optl_str +
+                 "  " + host_str +
                  " order by compiler, optl, switches")
-        scores.append(extractor.run_query(db, quers))
-        x_axis = extractor.run_query(db, quers)
-        quers = ("select distinct score0, switches, " +
-                 "compiler, optl from " +
-                 " tests where run = " + str(args.run) +
-                 " and name = '" + t['name'] + "'" + prec_str + comp_str +
-                 " and optl = '-O1'" +
-                 " order by compiler, optl, switches")
-        scores.append(extractor.run_query(db, quers))
-        quers = ("select distinct score0, switches, " +
-                 "compiler, optl from " +
-                 " tests where run = " + str(args.run) +
-                 " and name = '" + t['name'] + "'" + prec_str + comp_str +
-                 " and optl = '-O2'" +
-                 " order by compiler, optl, switches")
-        scores.append(extractor.run_query(db, quers))
-        quers = ("select distinct score0, switches, " +
-                 "compiler, optl from " +
-                 " tests where run = " + str(args.run) +
-                 " and name = '" + t['name'] + "'" + prec_str + comp_str +
-                 " and optl = '-O3'" +
-                 " order by compiler, optl, switches")
-        scores.append(extractor.run_query(db, quers))
-        eq_classes = []
-        del eq_classes[:]
+        scores = extractor.run_query(db, quers)
+        scores = [x['score0'] for x in scores]
+        eq_classes = {}
         line_classes = []
-        for set in scores:
-            color = 0
-            eq = {}
-            for x in set:
-                if not x['score0'] in eq:
-                    eq[x['score0']] = color
-                    color += 1
-            eq_classes.append(eq)
+        eq_classes = OrderedDict(zip(scores, scores))
+        color = 0
+        for key in eq_classes:
+            eq_classes[key] = color
+            color += 1
+
         for x in x_axis:
-            quer = ("select distinct score0, optl from tests where name = '" +
-                    t['name'] + "' and precision = '" + args.precision +
-                    "' and switches = '" + x['switches'] +
-                    "' and compiler = '" + x['compiler'] +
-                    "' and run = " + str(args.run) + " order by optl")
+            quer = ("select" +
+                    " distinct" +
+                    "  score0" +
+                    " from tests where" +
+                    "  name = '{0}'".format(t['name']) +
+                    "  and precision = '{0}'".format(x['precision']) +
+                    "  and switches = '{0}'".format(x['switches']) +
+                    "  and optl = '{0}'".format(x['optl']) +
+                    "  and run = {0}".format(args.run) +
+                    "  and host = '{0}'".format(x['host']))
             score = extractor.run_query(db, quer)
-            #assert len(score) == 4, score
             x_count += 1
-            test_scores = []
-            for de in zip(score, eq_classes):
-                test_scores.append(de[1][de[0]['score0']])
-            line_classes.append(test_scores)
+            line_classes.append(eq_classes[score[0]['score0']])
         z_data.append(line_classes)
 
     plot(x_ticks, y_ticks, z_data, args.output,
