446bdde29b7e77f80407c335f7e4ea3c7ed61bf4
Geof23 <fredericflintstone@gmail.com>
2017-03-13 15:34:28 -0600

    Woops -- I had a hard run # set for debugging.  Removed

--- scripts/hostfile.py
+++ scripts/hostfile.py
@@ -9,13 +9,14 @@ import multiprocessing
 user = os.environ['USER']
 cores = multiprocessing.cpu_count()
 
-DB_HOST = ('sawaya', 'bihexal.cs.utah.edu') 
-RUN_HOSTS = (('u0422778', 'kingspeak1.chpc.utah.edu', 56,
-              'kingspeak_cpu_startup', False, False),
-             ('u0422778', 'kingspeak1.chpc.utah.edu', 12,
-              'kingspeak_gpu_startup', True, False),
-             ('sawaya', 'ms0138.utah.cloudlab.us', 8,
-              None, False, False),)
+DB_HOST = (user, 'localhost') 
+#DB_HOST = ('sawaya', 'bihexal.cs.utah.edu') 
+# RUN_HOSTS = (('u0422778', 'kingspeak1.chpc.utah.edu', 56,
+#               'kingspeak_cpu_startup', False, False),
+#              ('u0422778', 'kingspeak1.chpc.utah.edu', 12,
+#               'kingspeak_gpu_startup', True, False),
+#              ('sawaya', 'ms0138.utah.cloudlab.us', 8,
+#               None, False, False),)
 
 #another possibility:
-#RUN_HOSTS = ((user, 'localhost', cores, None, False, False),)
+RUN_HOSTS = ((user, 'localhost', cores, None, False, False),)
