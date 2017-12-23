137b82f2c9c9cd9340ac2849ebd8559c771f0c6d
Michael Bentley <mikebentley15@gmail.com>
2017-12-22 17:21:01 -0700

    bisect: Extract variables from Makefile

--- scripts/flitcli/flit_bisect.py
+++ scripts/flitcli/flit_bisect.py
@@ -16,11 +16,6 @@ import sys
 
 brief_description = 'Bisect compilation to identify problematic source code'
 
-def _file_check(filename):
-    if not os.path.isfile(filename):
-        raise argparse.ArgumentTypeError('File does not exist: {0}'.format(filename))
-    return filename
-
 def main(arguments, prog=sys.argv[0]):
     parser = argparse.ArgumentParser(
             prog=prog,
@@ -33,7 +28,7 @@ def main(arguments, prog=sys.argv[0]):
                 reproducibility problem lies.
                 ''',
             )
-    parser.add_argument('-c', '--compilation',
+    parser.add_argument('compilation',
                         help='''
                             The problematic compilation to use.  This should
                             specify the compiler, optimization level, and
@@ -44,6 +39,8 @@ def main(arguments, prog=sys.argv[0]):
                             into three groups using space separators, the first
                             is the compiler, the second is the optimization
                             level, and the third (if present) is the switches.
+                            You will likely want to have this argument in
+                            quotes since it will contain spaces.
                             ''')
     args = parser.parse_args(arguments)
 
@@ -57,98 +54,27 @@ def main(arguments, prog=sys.argv[0]):
 
     # Split the compilation into the separate components
     compiler, optl, switches = args.compilation.strip().split(maxsplit=2)
-    print('compiler: ', repr(compiler))
-    print('optl:     ', repr(optl))
-    print('switches: ', repr(switches))
 
     # TODO: see if the Makefile needs to be regenerated
-    # TODO: get the list of source files from the Makefile
+
+    # get the list of source files from the Makefile
+    sources = util.extract_make_var('SOURCE', 'Makefile')
+
     # TODO: determine if the problem is on the linker's side
-    # TODO:   I'm not yet sure the best way to do that
+    #         I'm not yet sure the best way to do that
+
     # TODO: Perform in parallel binary search from ground-truth and from
     #       problematic
+    # - create extra Makefile
+    # - run extra Makefile to determine if the problem is still there
+    # - 
+
     # TODO: Use the custom comparison function in the test class when
     #       performing the binary search.
+
     # TODO: autogenerate Makefiles in the /tmp directly, preferrably with the
     #       tempfile module.
 
-    # Below was copied from flit_import
-    #assert projconf['database']['type'] == 'sqlite', \
-    #        'Only sqlite database supported'
-    #db = util.sqlite_open(projconf['database']['filepath'])
-
-    ## create a new run and set the args.append run id
-    #if args.append is None:
-    #    # Create a new run to use in import
-    #    db.execute('insert into runs(rdate,label) values (?,?)',
-    #            (datetime.datetime.now(), args.label))
-    #    db.commit()
-    #    args.append = db.execute('select id from runs order by id').fetchall()[-1]['id']
-
-    ## Make sure the run id exists.
-    #run_ids = [x['id'] for x in db.execute('select id from runs')]
-    #assert args.append in run_ids, \
-    #        'Specified append run id {0} is not in the runs ' \
-    #        'table'.format(args.append)
-
-    #for importee in args.importfile:
-    #    print('Importing', importee)
-    #    if util.is_sqlite(importee):
-    #        import_db = util.sqlite_open(importee)
-    #        cur = import_db.cursor()
-    #        cur.execute('select id from runs')
-    #        importee_run_ids = sorted([x['id'] for x in cur])
-    #        if len(importee_run_ids) == 0:
-    #            print('  no runs in database: nothing to import')
-    #            continue
-    #        latest_run = importee_run_ids[-1]
-    #        import_run = args.run if args.run is not None else latest_run
-    #        cur.execute('select name,host,compiler,optl,switches,precision,'
-    #                    'comparison,comparison_d,file,nanosec '
-    #                    'from tests where run = ?', (import_run,))
-    #        rows = [dict(x) for x in cur]
-    #    else:
-    #        with open(importee, 'r') as csvin:
-    #            reader = csv.DictReader(csvin)
-    #            rows = [row for row in reader]
-    #    if len(rows) == 0:
-    #        print('  zero rows: nothing to import')
-    #        continue
-    #    to_insert = []
-    #    for row in rows:
-    #        # Convert 'NULL' to None
-    #        for key, val in row.items():
-    #            row[key] = val if val != 'NULL' else None
-    #        # Insert
-    #        to_insert.append((
-    #            args.append,
-    #            row['name'],
-    #            row['host'],
-    #            row['compiler'],
-    #            row['optl'],
-    #            row['switches'],
-    #            row['precision'],
-    #            row['comparison'],
-    #            row['comparison_d'],
-    #            row['file'],
-    #            row['nanosec'],
-    #            ))
-    #    db.executemany('''
-    #        insert into tests(
-    #            run,
-    #            name,
-    #            host,
-    #            compiler,
-    #            optl,
-    #            switches,
-    #            precision,
-    #            comparison,
-    #            comparison_d,
-    #            file,
-    #            nanosec)
-    #        values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
-    #        ''', to_insert)
-    #db.commit()
 
 if __name__ == '__main__':
     sys.exit(main(sys.argv[1:]))
--- scripts/flitcli/flitutil.py
+++ scripts/flitcli/flitutil.py
@@ -6,7 +6,9 @@ import flitconfig as conf
 
 import os
 import sqlite3
+import subprocess as subp
 import sys
+import tempfile
 
 def process_in_file(infile, dest, vals, overwrite=False):
     '''
@@ -63,3 +65,38 @@ def is_sqlite(filename):
         header = fd.read(100)
 
     return header[:16] == b'SQLite format 3\000'
+
+def extract_make_var(var, makefile='Makefile'):
+    '''
+    Extracts the value of a particular variable within a particular Makefile.
+
+    How it works with a valid file:
+
+    >>> from tempfile import NamedTemporaryFile as NTF
+    >>> with NTF(mode='w+') as fout:
+    ...     print('A    := hello there sweetheart\\n', file=fout, flush=True)
+    ...     A = extract_make_var('A', fout.name)
+    >>> A
+    ['hello', 'there', 'sweetheart']
+
+    If the variable is undefined, then simply an empty list is returned.
+
+    >>> with NTF() as fout: extract_make_var('A', fout.name)
+    []
+
+    What if the file does not exist?  It throws an exception:
+
+    >>> extract_make_var('A', 'file-should-not-exist.mk') # doctest: +ELLIPSIS
+    Traceback (most recent call last):
+    ...
+    subprocess.CalledProcessError: Command ... returned non-zero exit status 2.
+    '''
+    with tempfile.NamedTemporaryFile(mode='w+') as fout:
+        print('print-%:\n'
+              "\t@echo '$*=$($*)'\n", file=fout, flush=True)
+        output = subp.check_output(['make', '-f', makefile, '-f', fout.name,
+                                    'print-' + var], stderr=subp.STDOUT)
+    output = output.strip().decode('utf-8')
+    var_values = output.split('=', maxsplit=1)[1].split()
+    return var_values
+
