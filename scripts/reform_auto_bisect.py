c128206cb9c4bc65d48a69a790eedd4f29bc9eb0
Ian Briggs <ianbriggsutah@gmail.com>
2018-10-15 12:20:16 -0600

    now a state machine

--- scripts/reform_auto_bisect.py
+++ scripts/reform_auto_bisect.py
@@ -6,7 +6,7 @@ import re
 for fn in glob.glob("output/test_*/bisect-*/bisect.log"):
 
     m = re.match("output/test_([0-9]*)/bisect-([0-9]*)/bisect.log", fn)
-    testcase = "ex{}_Test".format(m.group())
+    testcase = "ex{}_Test".format(m.group(1))
     bisect_num = m.group(2)
 
     compiler = None
@@ -18,88 +18,120 @@ for fn in glob.glob("output/test_*/bisect-*/bisect.log"):
     srcs = None
     syms = None
 
-    status = 0
+    status = 1
 
-    look_for_libs = False
-    DEFAULT = 1
-    LIBS = 2
-    SRC = 3
-    
-    
-    STATE = default
-    
+    INTRO = 1
+    LIB_START = 2
+    LIB = 3
+    SRC_START = 4
+    SRC = 5
+    SYM_START = 6
+    SYM = 7
+    state = INTRO
+
     with open(fn, "r") as f:
         for line in f:
-            assert(line != None)
-            # ...: Starting the bisect procedure
-            # ...:   trouble compiler:           "g++"
-            # ...:   trouble optimization level: "-O2"
-            # ...:   trouble switches:           "-mavx2 -mfma"
-            m = re.search(r"trouble compiler:           \"(.*)\"", line)
-            if m != None:
-                compiler = m.group(1)
-                continue
 
-            m = re.search(r"trouble optimization level: \"(.*)\"", line)
-            if m != None:
-                optl = m.group(1)
-                continue
+            if state == INTRO:
+                # ...: Starting the bisect procedure
+                # ...:   trouble compiler:           "g++"
+                # ...:   trouble optimization level: "-O2"
+                # ...:   trouble switches:           "-mavx2 -mfma"
+                m = re.search(r"trouble compiler:           \"(.*)\"", line)
+                if m != None:
+                    compiler = m.group(1)
+                    continue
 
-            m = re.search(r"trouble switches:           \"(.*)\"", line)
-            if m != None:
-                switches = m.group(1)
-                if compiler == "icpc":
-                    switches = switches.split(maxsplit=1)[1]
-                    optl = switches.split(maxsplit=1)[0]
-                    try:
+                m = re.search(r"trouble optimization level: \"(.*)\"", line)
+                if m != None:
+                    optl = m.group(1)
+                    continue
+
+                m = re.search(r"trouble switches:           \"(.*)\"", line)
+                if m != None:
+                    switches = m.group(1)
+                    if compiler == "icpc":
                         switches = switches.split(maxsplit=1)[1]
-                    except IndexError:
-                        switches = ""
+                        optl = switches.split(maxsplit=1)[0]
+                        try:
+                            switches = switches.split(maxsplit=1)[1]
+                        except IndexError:
+                            switches = ""
+                    state = LIB_START
+                    continue
+
                 continue
 
-            if line.count("BAD STATIC LIBRARIES:") >= 1:
-                look_for_libs = True
-                libs = list()
+
+            if state == LIB_START:
+                if "BAD STATIC LIBRARIES:" in line:
+                    state = LIB
+                    libs = list()
+                    continue
+
+                if "ALL VARIABILITY INCUDING SOURCE FILE(S):" in line:
+                    state = SRC
+                    libs = list()
+                    srcs = list()
+                    continue
+
                 continue
 
-            if look_for_libs:
-                if line.count("None") >= 1 or line.count("May not be able to search further, because of intel optimizations") >= 1:
-                    look_for_libs = False
+
+            if state == LIB:
+                msg = "May not be able to search further, because of intel optimizations"
+                if line.count("None") >= 1 or line.count(msg) >= 1:
+                    state = SRC_START
                 else:
                     lib = line.split()[-1]
                     libs.append(lib)
+
                 continue
 
-            # Found differing source file /uufs/chpc.utah.edu/common/home/u0692013/FLIT/FLiT_mfem/mfem/linalg/sparsemat.cpp: score 3.452641112552743e-14
-            # ALL VARIABILITY INCUDING SOURCE FILE(S)
-            m = re.search("Found differing source file (.*): score", line)
-            if 'ALL VARIABILITY INCUDING SOURCE FILE(S)' in line:
-                if srcs == None:
+
+            if state == SRC_START:
+                if "ALL VARIABILITY INCUDING SOURCE FILE(S):" in line:
+                    state = SRC
                     srcs = list()
-                srcs.append(m.group(1))
+                    continue
+
                 continue
 
-            # Found differing symbol on line 493 -- mfem::SparseMatrix::AddMult(mfem::Vector const&, mfem::Vector&, double) const (score 3.452641112552743e-14)
-            m = re.search("Found differing symbol on line [0-9]* -- (.*) \(score", line)
-            if m != None:
-                if syms == None:
+
+            if state == SRC:
+                msg = "Searching for differing symbols in"
+                if line.count("None") >= 1 or line.count(msg) >= 1:
+                    state = SYM_START
+                else:
+                    assert(line != "(score")
+                    src = line[line.index("bisect:") + 7 : line.index("(score")]
+                    src = src.strip()
+                    srcs.append(src)
+
+                continue
+
+
+            if state == SYM_START:
+                if "ALL VARIABILITY INCUDING SYMBOLS:" in line:
+                    status = 0
+                    state = SYM
                     syms = list()
-                syms.append(m.group(1))
+                    continue
+
                 continue
 
 
-            if line.count("cannot continue") >= 1:
-                status = 1
-                if syms != None:
-                    syms = None
+            if state == SYM:
+                if "None" in line:
                     break
-                if srcs != None:
-                    srcs = None
-                    break
-                if libs != None:
-                    libs = None
-                    break
+                m = re.search("\.cpp:[0-9]* (.*) \(score", line)
+                assert(m != None)
+                sym = m.group()[1]
+                syms.append(sym)
 
+                continue
+
+            assert(0)
 
     # bisectnum,compiler,optl,switches,precision,testcase,type,name,return
     completed = list()
@@ -119,7 +151,7 @@ for fn in glob.glob("output/test_*/bisect-*/bisect.log"):
 
     if srcs != None:
         completed.append("src")
-        for src in srcs:
+        for source in srcs:
             print('{},{},{},"{}",{},{},{},"{}",{}'.format(
                 bisect_num,
                 compiler,
@@ -128,7 +160,7 @@ for fn in glob.glob("output/test_*/bisect-*/bisect.log"):
                 precision,
                 testcase,
                 "src",
-                src,
+                source,
                 status))
 
     if syms != None:
