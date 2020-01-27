9824d9b27f11c8a5ec35ad589ec4c3ab10f8a22e
Michael Bentley <mikebentley15@gmail.com>
2020-01-27 11:05:42 -0700

    Add all_flit_src.cpp

---
+++ src/all_flit_src.cpp
@@ -0,0 +1,13 @@
+// This file includes all of the source files within FLiT to be compiled in one go
+//
+// For now, this is an experiment to see how it affects compile-time
+
+#include "flit.cpp"
+#include "FlitCsv.cpp"
+#include "flitHelpers.cpp"
+#include "fsutil.cpp"
+#include "InfoStream.cpp"
+#include "subprocess.cpp"
+#include "TestBase.cpp"
+#include "timeFunction.cpp"
+#include "Variant.cpp"
